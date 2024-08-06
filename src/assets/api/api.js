6fb5c45ab36fdf2ac24af3849ec8503d52956e5f
R.Chagas <renatochagas.m@gmail.com>
2024-08-06 12:05:40 -0300

    problem line 1 api.js

--- src/assets/api/api.js
+++ src/assets/api/api.js
@@ -1,1 +1,24 @@
-import { Axios } from 'axios';
+import { axios } from 'axios'
+import { ref,  onMounted  } from 'vue'
+
+const moviesGenres = ref([])
+const tvGenres = ref([])
+
+onMounted(async () => {
+  let response = await axios.get('https://api.themoviedb.org/3/genre/movie/list?language=pt-BR', {
+    headers: {
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYWI0M2JkNjcxZjkzZWVmMDA2YzA5MDU3MzY1ZTIxNCIsIm5iZiI6MTcyMjkxMTQ0MS40MTMyMTUsInN1YiI6IjY2YjE4OWNjZWFkN2M0ZTgyMzI1ZmVlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uRiXL_s8BVxlmy-OzEJD-xkR-UTvMsoO-zGzY1ZOSmg`
+    },
+  },
+);
+    moviesGenres.value = response.data.genres;
+    response = await axios.get(
+        'https://api.themoviedb.org/3/genre/tv/list?language=pt-BR',
+        {
+            headers:{
+                Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYWI0M2JkNjcxZjkzZWVmMDA2YzA5MDU3MzY1ZTIxNCIsIm5iZiI6MTcyMjkxMTQ0MS40MTMyMTUsInN1YiI6IjY2YjE4OWNjZWFkN2M0ZTgyMzI1ZmVlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uRiXL_s8BVxlmy-OzEJD-xkR-UTvMsoO-zGzY1ZOSmg'
+            },
+        },
+    );
+    tvGenres.value = response.data.genres;
+})
---
+++ src/views/Pageview.vue
@@ -0,0 +1,27 @@
+<script setup>
+import API from '@/assets/api/api.js'
+
+mGtvG = API
+</script>
+
+<template>
+    <template>
+        <h1>Gêneros de filmes</h1>
+        <ul>
+          <li v-for="genre in moviesGenres" :key="genre.id">
+            {{ genre.name }} 
+          </li>
+        </ul>
+        <hr />
+        <h1>Gêneros de programas de TV</h1>
+        <ul>
+          <li v-for="genre in TVGenres" :key="genre.id">
+            {{ genre.name }}
+          </li>
+        </ul>
+      </template>
+</template>
+
+<style scoped>
+
+</style>
